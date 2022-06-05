614395b4739614ba0568ed7247ca8bf5e26559d3
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2022-06-06 00:58:28 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>Bazaar@1.0.0 by andyjjrt</h1>
+    <h1>Bazaar@1.0.1</h1>
+    <h2>Frontend+Backend: andyjjrt</h2>
+    <h2>some smol changes: AiverAiva</h2>
   </div>
 </template>
