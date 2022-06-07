0a06e9c58eb9d23dfb557830d6d42da9f6446987
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2022-06-07 09:13:15 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,22 +2,43 @@
   <div class="about">
     <h1>Bazaar@1.0.1</h1>
     <h2><a href="https://andyjjrt.github.io/">Frontend+Backend: andyjjrt</a></h2>
-    <h2><a href="weikuwu.ga">some smol changes: AiverAiva</a></h2>
+    <h2><a href="https://weikuwu.ga">some smol changes: AiverAiva</a></h2>
     <v-row>
       <v-col cols="12" sm="6" md="6" lg="6">
-        <v-alert border="bottom">
-          <div class="text-h5">
-            aaa
-          </div>
-        </v-alert>
+        <template>
+          <v-card class="mx-auto" max-width="344" outlined>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  DEV WHO DID NOTHING
+                </div>
+                <v-list-item-title class="text-h5 mb-1">
+                  AiverAiva
+                </v-list-item-title>
+                <v-list-item-subtitle>owo uwu</v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-avatar size="80" color="grey"><img src="https://avatars.githubusercontent.com/u/43096905?v=4" alt="AiverAiva"></v-list-item-avatar>
+            </v-list-item>
+
+            <v-card-actions>
+              <router-link to="https://github.com/AiverAiva">
+                <v-btn outlined rounded text>
+                  <v-icon>fa-brands fa-github</v-icon>
+                </v-btn>
+              </router-link>
+
+            </v-card-actions>
+          </v-card>
+        </template>
       </v-col>
       <v-col cols="12" sm="6" md="6" lg="6">
-        <v-alert border="bottom">
-          <div class="text-h5">
-            bbb
-          </div>
-        </v-alert>
+
       </v-col>
     </v-row>
   </div>
 </template>
+
+<script>
+export default {}
+</script>
