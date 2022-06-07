7cf1ab7afb2baba56d01ff864d650d9b87d3d663
WeiKuOuO <43096905+AiverAiva@users.noreply.github.com>
2022-06-07 08:47:21 +0800

    Update About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="about">
     <h1>Bazaar@1.0.1</h1>
-    <h2><a :href="website">Frontend+Backend: andyjjrt</a></h2>
-    <h2><a :href="website">some smol changes: AiverAiva</a></h2>
+    <h2><a href="https://andyjjrt.github.io/">Frontend+Backend: andyjjrt</a></h2>
+    <h2><a href="weikuwu.ga">some smol changes: AiverAiva</a></h2>
     <v-row>
-      aaaa
+      <v-col cols="12" sm="6" md="6" lg="6">
+        <v-alert border="bottom">
+          <div class="text-h5">
+            aaa
+          </div>
+        </v-alert>
+      </v-col>
+      <v-col cols="12" sm="6" md="6" lg="6">
+        <v-alert border="bottom">
+          <div class="text-h5">
+            bbb
+          </div>
+        </v-alert>
+      </v-col>
     </v-row>
   </div>
 </template>
